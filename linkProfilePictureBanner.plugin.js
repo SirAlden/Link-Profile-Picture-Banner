ca1f4fa1c29bd7c80499cc8190719a3d5fa06c76
siralden <37455371+SirAlden@users.noreply.github.com>
2022-09-20 23:48:59 -0400

    Updated banner grabber code

--- linkProfilePictureBanner.plugin.js
+++ linkProfilePictureBanner.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name Link-Profile-Picture-Banner
  * @description Lets you click users' avatars and banners on their profile page to view a bigger version in your browser.
- * @version 2.0.0
+ * @version 2.1.0
  * @author Broken Satellite Network
  * @source https://github.com/SirAlden/Link-Profile-Picture-Banner/blob/main/linkProfilePictureBanner.plugin.js
  * @updateUrl https://raw.githubusercontent.com/SirAlden/Link-Profile-Picture-Banner/main/linkProfilePictureBanner.plugin.js
@@ -18,11 +18,11 @@ module.exports = class linkProfilePicture {
         if (target.classList.contains("avatar-3QF_VA")) {
 			window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
 		}
-        else if (target.style.backgroundImage && target.style.backgroundImage.includes("banner")) {
+		else if (target.classList.contains("banner-2boKnS")) {
             let url = target.style.backgroundImage
             url = url.substring(4, url.length-1).replace(/["']/g, "")
             url = url.replace(/(?:\?size=\d{3,4})?$/, "?size=4096")
             window.open(url)
-        }
+		}
     }
 }
