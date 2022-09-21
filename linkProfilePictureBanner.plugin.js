bc5897d2006643f65d9276241afc699ad7973303
siralden <37455371+SirAlden@users.noreply.github.com>
2022-09-20 23:51:00 -0400

    Add files via upload

--- linkProfilePictureBanner.plugin.js
+++ linkProfilePictureBanner.plugin.js
@@ -8,21 +8,21 @@
  */
 
 module.exports = class linkProfilePicture {
-    start() {
-        document.addEventListener("click", this.link)
-    }
-    stop() {
-        document.removeEventListener("click", this.link)
-    }
-    link({target}) {
-        if (target.classList.contains("avatar-3QF_VA")) {
+	start() {
+		document.addEventListener("click", this.link)
+	}
+	stop() {
+		document.removeEventListener("click", this.link)
+	}
+	link({target}) {
+		if (target.classList.contains("avatar-3QF_VA")) {
 			window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
 		}
 		else if (target.classList.contains("banner-2boKnS")) {
-            let url = target.style.backgroundImage
-            url = url.substring(4, url.length-1).replace(/["']/g, "")
-            url = url.replace(/(?:\?size=\d{3,4})?$/, "?size=4096")
-            window.open(url)
+			let url = target.style.backgroundImage
+			url = url.substring(4, url.length-1).replace(/["']/g, "")
+			url = url.replace(/(?:\?size=\d{3,4})?$/, "?size=4096")
+			window.open(url)
 		}
-    }
+	}
 }
